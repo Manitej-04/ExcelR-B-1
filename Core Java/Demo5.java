fd1b99c640115d54757eee93e289f8ed6393771e
Manitej-04 <151040895+Manitej-04@users.noreply.github.com>
2025-01-04 12:20:11 +0530

    Add files via upload

---
+++ Core Java/Demo5.java
@@ -0,0 +1,11 @@
+public class Demo5 {
+    public static void main(String[] args) {
+        String username = "Mani";
+        int password = 2005;
+
+        while((username=="Mani")&&(password==2005)){ 
+                System.out.println("User logged in");
+                break;
+        }
+    }
+}
