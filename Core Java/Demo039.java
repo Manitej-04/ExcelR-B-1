ac94a725751978f944357366528126538b1af99a
Manitej-04 <151040895+Manitej-04@users.noreply.github.com>
2025-01-24 21:18:45 +0530

    Add files via upload

---
+++ Core Java/Demo039.java
@@ -0,0 +1,12 @@
+public class Demo039 {
+    public static void main(String[] args) {
+        int x = 7;
+
+        if(Character.isUpperCase(x)){
+            System.out.println("x is uppercase");
+        } else if(Character.isLowerCase(x)){
+            System.out.println("x is lower case");
+        } else if()
+
+    }
+}
