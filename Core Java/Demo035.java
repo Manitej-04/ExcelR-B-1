1a2ed5d7f7f1e432463afd069d84409ed43af6af
Manitej-04 <151040895+Manitej-04@users.noreply.github.com>
2025-01-24 21:19:40 +0530

    Greatest of 2 numbers

--- Core Java/Demo035.java
+++ Core Java/Demo035.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Demo035 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        
         System.out.print("Enter first number: ");
         int num1 = sc.nextInt();
         System.out.print("Enter second number: ");
