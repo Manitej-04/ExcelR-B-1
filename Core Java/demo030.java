ac87fbbc45793dfaee6701f7b7e500c76dbe8f3c
Manitej-04 <151040895+Manitej-04@users.noreply.github.com>
2025-01-25 11:40:25 +0530

    ArrayList

---
+++ Core Java/demo030.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class ArrayListCode{
+	void arrayListMethod(){  
+		ArrayList<Integer> arr = new ArrayList<>();
+
+		arr.add(10);
+		arr.add(20);
+		arr.add(30);
+		arr.add(40);
+		arr.add(50);
+
+		Iterator<Integer> i = arr.iterator();
+		while(i.hasNext()){
+			System.out.println(i.next());
+		}
+
+	}
+
+}
+public class demo030{
+
+	public static void main(String[] args) {
+		ArrayListCode obj = new ArrayListCode();
+		obj.arrayListMethod();
+		}
+	}
+
