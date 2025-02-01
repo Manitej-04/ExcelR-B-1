0833f98f1a60eb0441be4d28ce9b71e9f248bc23
Manitej-04 <151040895+Manitej-04@users.noreply.github.com>
2025-02-01 11:15:41 +0530

    Taking User details via input

--- Core Java/Demo007.java
+++ Core Java/Demo007.java
@@ -1,13 +1,20 @@
-class demo7 {
-    @SuppressWarnings("StringEquality")
+import java.util.Scanner;
+
+class Demo007 {
     public static void main(String[] args) {
-
-        String Username = "Mani";
-        String Password = "Mani417";
-
-        while((Username == "Mani") && (Password == "Mani417")) {
-            System.out.println("Welcome to Mallareddy university");
-            break;
-        }
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter your name: ");
+            String name = sc.nextLine();
+            
+            System.out.print("Enter your age: ");
+            int age = sc.nextInt();
+            
+            System.out.println("Enter your phone number: ");
+            long num = sc.nextLong();
+            
+            System.out.println("Name: "+name);
+            System.out.println("Age: "+age);
+            System.out.println("Phone Number: "+num);
+        
     }
  }
